26952729676516cc3b249ae70358d1eadd35707d
Rutvij Khatri <rutvij.khatri@gmail.com>
2019-03-10 13:20:10 +0530

    -Cannot add user with empty fields -Username min length 3 characters -Password min length 6 characters -Removed redundancy for EditText,TextView and Button

--- app/src/main/java/com/example/myapp/signup.java
+++ app/src/main/java/com/example/myapp/signup.java
@@ -44,8 +44,8 @@ public class signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        edit_username = (EditText) findViewById(R.id.username);
-        edit_dob = (TextView) findViewById(R.id.dob);
+        edit_username = findViewById(R.id.username);
+        edit_dob = findViewById(R.id.dob);
 
 
         edit_dob.setOnClickListener(new View.OnClickListener() {
@@ -74,8 +74,8 @@ public class signup extends AppCompatActivity {
             }
         };
 
-        edit_pass = (EditText) findViewById(R.id.pass);
-        btn_createAcc = (Button) findViewById(R.id.createAcc);
+        edit_pass = findViewById(R.id.pass);
+        btn_createAcc = findViewById(R.id.createAcc);
 
         btn_createAcc.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,21 +92,31 @@ public class signup extends AppCompatActivity {
                 usr.put(KEY_PASSWORD,user.getPassword());
                 userRef = db.collection("ExpenseTracker").document("user "+user.getUsername());
 
-
-                userRef.set(usr)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                Toast.makeText(signup.this, "Success", Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(signup.this, "Failed to register", Toast.LENGTH_SHORT).show();
-                                Log.d(TAG,e.toString());
-                            }
-                        });
+                if (user.getDob().isEmpty() || user.getUsername().isEmpty() || user.getPassword().isEmpty()){
+                    Toast.makeText(signup.this, "Make sure none of the field is empty", Toast.LENGTH_SHORT).show();
+                }
+                else if (user.getUsername().length() < 3){
+                    Toast.makeText(signup.this, "Minimum username length should be 3 characters", Toast.LENGTH_SHORT).show();
+                }
+                else if (user.getPassword().length() < 6){
+                    Toast.makeText(signup.this, "Minimum pass length is 6 characters", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    userRef.set(usr)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Toast.makeText(signup.this, "Success", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(signup.this, "Failed to register", Toast.LENGTH_SHORT).show();
+                                    Log.d(TAG,e.toString());
+                                }
+                            });
+                }
 
             }
         });
