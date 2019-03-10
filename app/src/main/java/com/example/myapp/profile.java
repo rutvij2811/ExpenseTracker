69b6878238623ce9529292262536d3912acb1182
Rutvij Khatri <rutvij.khatri@gmail.com>
2019-03-10 17:14:08 +0530

    -Calendar Dialog Picker for Profile Management Activity

--- app/src/main/java/com/example/myapp/profile.java
+++ app/src/main/java/com/example/myapp/profile.java
@@ -1,5 +1,6 @@
 package com.example.myapp;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,30 +20,57 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.SetOptions;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class profile extends AppCompatActivity {
-    EditText et_dob, et_phone, et_addr;
+    EditText et_phone, et_addr;
     private Button back, btn_save;
     private static final String TAG = "profile";
     private DocumentReference loginRef,userRef;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    TextView tv_username;
+    TextView tv_username,et_dob;
     private static final String KEY_PHONE ="phone";
     private static final String KEY_ADDR = "address";
     private static final String KEY_DOB = "dob";
+    DatePickerDialog.OnDateSetListener mDateSetListner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        et_dob = (EditText) findViewById(R.id.pro_dob);
-        et_phone = (EditText) findViewById(R.id.pro_phone);
-        et_addr = (EditText) findViewById(R.id.pro_addr);
-        tv_username =(TextView) findViewById(R.id.user_pro);
-        btn_save = (Button) findViewById(R.id.pro_save);
+        et_dob =  findViewById(R.id.pro_dob);
+        et_dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        profile.this,
+                        android.R.style.Theme_DeviceDefault_Light_Dialog,
+                        mDateSetListner,
+                        year, month, day);
+                dialog.show();
+            }
+        });
+        mDateSetListner = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month + 1;
+                String dob = dayOfMonth + "-" + month + "-" + year;
+                et_dob.setText(dob);
+
+            }
+        };
+        et_phone =  findViewById(R.id.pro_phone);
+        et_addr = findViewById(R.id.pro_addr);
+        tv_username =findViewById(R.id.user_pro);
+        btn_save =  findViewById(R.id.pro_save);
         loginRef = db.collection("login").document("username");
         loginRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -71,7 +100,7 @@ public class profile extends AppCompatActivity {
             }
         });
 
-        back = (Button) findViewById(R.id.back);
+        back =  findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
