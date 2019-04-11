f340cd94fadd41d5437bf9f83aab31a5c3fa5c8e
Rutvij Khatri <rutvij.khatri@gmail.com>
2019-04-11 16:12:47 +0530

    -Text to speech in ForgotPassword activity

--- app/src/main/java/com/example/myapp/ForgotPassword.java
+++ app/src/main/java/com/example/myapp/ForgotPassword.java
@@ -3,6 +3,7 @@ package com.example.myapp;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,16 +18,26 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Locale;
+
 public class ForgotPassword extends AppCompatActivity {
     private EditText et_fp_user,et_fp_secret;
     private Button btn_verify;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private TextToSpeech mTTS;
     private DocumentReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        mTTS =new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                mTTS.setLanguage(Locale.ENGLISH);
+            }
+        });
+
 
         et_fp_user = findViewById(R.id.et_fp_user);
         et_fp_secret = findViewById(R.id.et_fp_secret);
@@ -36,7 +47,11 @@ public class ForgotPassword extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(et_fp_user.getText().toString().isEmpty() || et_fp_secret.getText().toString().isEmpty()){
-                    Toast.makeText(ForgotPassword.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(ForgotPassword.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+                    String text = "Please fill all the fields.";
+                    mTTS.setPitch(1);
+                    mTTS.setSpeechRate(1);
+                    mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                 }
                 else{
 
@@ -55,12 +70,20 @@ public class ForgotPassword extends AppCompatActivity {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         if(!documentSnapshot.exists()){
-                            Toast.makeText(ForgotPassword.this, "No such user found", Toast.LENGTH_SHORT).show();
+//                            Toast.makeText(ForgotPassword.this, "No such user found", Toast.LENGTH_SHORT).show();
+                            String text = "No such user found.";
+                            mTTS.setPitch(1);
+                            mTTS.setSpeechRate(1);
+                            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                         }
                         String userSecret = et_fp_secret.getText().toString();
                         String actualSecret = documentSnapshot.getString("secret");
                         if(userSecret.equalsIgnoreCase(actualSecret)){
-                            Toast.makeText(ForgotPassword.this, "User Verified", Toast.LENGTH_SHORT).show();
+//                            Toast.makeText(ForgotPassword.this, "User Verified", Toast.LENGTH_SHORT).show();
+                            String text = "User verified.";
+                            mTTS.setPitch(1);
+                            mTTS.setSpeechRate(1);
+                            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                             SharedPreferences sharedPref = getSharedPreferences("userVerify", Context.MODE_PRIVATE);
 
                             SharedPreferences.Editor editor =sharedPref.edit();
