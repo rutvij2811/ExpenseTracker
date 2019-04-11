67ede472e60d8ec2add57ea67bd41f3434642001
Rutvij Khatri <rutvij.khatri@gmail.com>
2019-04-11 16:13:53 +0530

    -Text to speech in home activity

--- app/src/main/java/com/example/myapp/home.java
+++ app/src/main/java/com/example/myapp/home.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -36,6 +37,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.annotation.Nullable;
@@ -49,6 +51,7 @@ public class home extends AppCompatActivity {
     private static final String KEY_DATE = "date";
     private static final String TAG = "home";
     private String userToUse;
+    private TextToSpeech mTTS;
     DatePickerDialog.OnDateSetListener mDateSetListner1;
     private Button logout, settings, profile, manlog, analysis, addRec,msg;
     private EditText et_cat, et_amt;
@@ -65,6 +68,12 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getLoginDetails();
         setContentView(R.layout.activity_home);
+        mTTS =new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                mTTS.setLanguage(Locale.ENGLISH);
+            }
+        });
         et_cat = findViewById(R.id.home_cat);
         //To make sp_cat spinner by getting the list of categories. First work on adding categories.
         et_amt = findViewById(R.id.home_amt);
@@ -165,7 +174,11 @@ public class home extends AppCompatActivity {
     private void addRecord() {
         String catName = et_cat.getText().toString();
         if (et_cat.getText().toString().isEmpty() || et_amt.getText().toString().isEmpty() || et_date.getText().toString().isEmpty()) {
-            Toast.makeText(this, "Make sure none of the fields are empty", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "Make sure none of the fields are empty", Toast.LENGTH_SHORT).show();
+            String text = "Make sure none of the fields are empty.";
+            mTTS.setPitch(1);
+            mTTS.setSpeechRate(1);
+            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
         } else {
             recordRef = db.collection(userToUse + " Record").document();
             Map<String, Object> insertRecord = new HashMap<>();
@@ -173,14 +186,22 @@ public class home extends AppCompatActivity {
             insertRecord.put(KEY_AMT, Integer.parseInt(et_amt.getText().toString()));
             insertRecord.put(KEY_DATE, getDateFromString(et_date.getText().toString()));
             if (!categoryList.contains(catName)) {
-                Toast.makeText(this, "Category not in the database, please add it first.", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(this, "Category not in the database, please add it first.", Toast.LENGTH_SHORT).show();
+                String text = "Category not in the database, please add it first.";
+                mTTS.setPitch(1);
+                mTTS.setSpeechRate(1);
+                mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
             } else {
                 recordRef
                         .set(insertRecord)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
-                                Toast.makeText(home.this, "Record added", Toast.LENGTH_SHORT).show();
+//                                Toast.makeText(home.this, "Record added", Toast.LENGTH_SHORT).show();
+                                String text = "Record added";
+                                mTTS.setPitch(1);
+                                mTTS.setSpeechRate(1);
+                                mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                                 et_amt.setText(null);
                                 et_date.setText(null);
                                 et_cat.setText(null);
