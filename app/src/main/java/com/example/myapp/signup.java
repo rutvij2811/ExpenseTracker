6c7de18309927748b040b4debbce6c5759477200
Rutvij Khatri <rutvij.khatri@gmail.com>
2019-04-11 16:58:31 +0530

    -Text to speech in signup activity

--- app/src/main/java/com/example/myapp/signup.java
+++ app/src/main/java/com/example/myapp/signup.java
@@ -3,6 +3,7 @@ package com.example.myapp;
 import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,6 +29,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.annotation.Nullable;
@@ -42,7 +44,7 @@ public class signup extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DocumentReference userRef;
     private CollectionReference userNameRef;
-
+    private TextToSpeech mTTS;
     private ArrayList<String> userList = new ArrayList<>();
 
     DatePickerDialog.OnDateSetListener mDateSetListner;
@@ -57,6 +59,14 @@ public class signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mTTS =new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                mTTS.setLanguage(Locale.ENGLISH);
+            }
+        });
+
         edit_username = findViewById(R.id.username);
         edit_dob = findViewById(R.id.dob);
         secretAns = findViewById(R.id.secretAns);
@@ -90,7 +100,11 @@ public class signup extends AppCompatActivity {
                 month = month + 1;
                 String dob = dayOfMonth + "-" + month + "-" + year;
                 if(year > 2001){
-                    Toast.makeText(signup.this, "Min age req is 18", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(signup.this, "Min age req is 18", Toast.LENGTH_SHORT).show();
+                    String text = "Minimum age required is 18.";
+                    mTTS.setPitch(1);
+                    mTTS.setSpeechRate(1);
+                    mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                 } else{
 
                     edit_dob.setText(dob);
@@ -119,23 +133,43 @@ public class signup extends AppCompatActivity {
                 userRef = db.collection("ExpenseTracker").document("user "+user.getUsername());
 
                 if (user.getDob().isEmpty() || user.getUsername().isEmpty() || user.getPassword().isEmpty() || user.getSecret().isEmpty()){
-                    Toast.makeText(signup.this, "Make sure none of the field is empty", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(signup.this, "Make sure none of the field is empty", Toast.LENGTH_SHORT).show();
+                    String text = "Make sure none of the field is empty.";
+                    mTTS.setPitch(1);
+                    mTTS.setSpeechRate(1);
+                    mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                 }
                 else if (userList.contains(user.getUsername())){
-                    Toast.makeText(signup.this, "UserName already there in the database, please try a different username", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(signup.this, "Username already there in the database, please try a different username", Toast.LENGTH_SHORT).show();
+                    String text = "Username already there in the database, please try a different username.";
+                    mTTS.setPitch(1);
+                    mTTS.setSpeechRate(1);
+                    mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                 }
                 else if (user.getUsername().length() < 3){
-                    Toast.makeText(signup.this, "Minimum username length should be 3 characters", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(signup.this, "Minimum username length should be 3 characters", Toast.LENGTH_SHORT).show();
+                    String text = "Username should be atleast 3 characters long.";
+                    mTTS.setPitch(1);
+                    mTTS.setSpeechRate(1);
+                    mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                 }
                 else if (user.getPassword().length() < 6){
-                    Toast.makeText(signup.this, "Minimum pass length is 6 characters", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(signup.this, "Minimum pass length is 6 characters", Toast.LENGTH_SHORT).show();
+                    String text = "Password should be atleast 6 characters long.";
+                    mTTS.setPitch(1);
+                    mTTS.setSpeechRate(1);
+                    mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                 }
                 else{
                     userRef.set(usr)
                             .addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
-                                    Toast.makeText(signup.this, "Success", Toast.LENGTH_SHORT).show();
+//                                    Toast.makeText(signup.this, "Success", Toast.LENGTH_SHORT).show();
+                                    String text = "Success.";
+                                    mTTS.setPitch(1);
+                                    mTTS.setSpeechRate(1);
+                                    mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
                                     getUserList();
                                     edit_username.setText(null);
                                     edit_dob.setText(null);
@@ -183,6 +217,16 @@ public class signup extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+        }
+
+        super.onDestroy();
     }
 
 }
